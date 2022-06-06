6b48accccb81aa942051bb22b3ccd216cd599836
priyanshu10 <priyanshuparate@gmail.com>
2022-06-06 11:27:39 +0530

    drink watter automation

---
+++ drinkwater-tracker/water_tracker.py
@@ -0,0 +1,27 @@
+import time
+from playsound import playsound
+import pync
+import datetime
+
+
+def notify(message, title):
+    pync.notify(title=title, message=message, sound="ping")
+
+
+def track_watter():
+    soundPath = "/Users/priyanshuparate/projects/python/python_automations/drinkwater-tracker/water-filled-cup-9901.mp3"
+    glasscount = 8
+    currenthr = datetime.datetime.now().hour
+    while True:
+        if currenthr > 9 and currenthr < 11 and glasscount > 0:
+
+            notify("drink one glass of watter",
+                   f"Drink water,{glasscount} yet to drink")
+
+            glasscount -= 1
+            playsound(soundPath)
+            time.sleep(80*60)
+
+
+if __name__ == "__main__":
+    track_watter()
