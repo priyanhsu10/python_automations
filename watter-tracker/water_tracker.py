cd1f67c980150a7ecc288bac06e7068de95a3c9c
priyanshu10 <priyanshuparate@gmail.com>
2022-06-06 12:08:59 +0530

    add base condition

--- watter-tracker/water_tracker.py
+++ watter-tracker/water_tracker.py
@@ -9,13 +9,13 @@ def notify(message, title):
 
 
 def track_watter():
-    soundPath = "/Users/priyanshuparate/projects/python/python_automations/drinkwater-tracker/water-filled-cup-9901.mp3"
+    soundPath = "/Users/priyanshuparate/projects/python/python_automations/water-tracker/water-filled-cup-9901.mp3"
     glasscount = 8
     currenthr = datetime.datetime.now().hour
     while True:
-        if glasscount == 0:
+        if glasscount == 0 or currenthr > 23:
             break
-        if currenthr > 9 and currenthr < 11 and glasscount > 0:
+        if currenthr > 9 and currenthr < 23 and glasscount > 0:
 
             notify("drink one glass of watter",
                    f"Drink water,{glasscount} yet to drink")
